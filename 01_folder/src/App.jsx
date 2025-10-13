9e44bb51ce76a1f260cf8ec48f243b443c18ea90
Somnath Mondal <somnathmondal22012000@gmail.com>
2025-10-13 19:57:54 +0530

    01_folder

---
+++ 01_folder/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App(){
+  return (
+   <>
+     <div id="parent">
+      <h1 id="child1">hello h1</h1>
+      <h2 id="child2">Hello h2</h2>
+    </div>
+     
+   </>
+    
+  )
+}
+
+export default App
